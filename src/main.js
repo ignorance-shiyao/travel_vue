ebf22f60a9b95c38e7e0c13690b476e30f3bdd31
一个孤陋寡闻的胖子 <52310567+gulouguawen@users.noreply.github.com>
2019-10-23 10:41:01 +0800

    banner design updates

--- src/main.js
+++ src/main.js
@@ -4,16 +4,16 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
-import './assets/styles/border.css'
-import './assets/styles/iconfont.css'
+import 'styles/reset.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false;
 fastClick.attach(document.body);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  // ES6 中如果键和值相同可以省略一部分内容  如 router: router 可以写作router
+  // ES6 中如果键和值相同可以省略一部分内容  如 router: router 可以写作 router
   router,
   components: {App},
   template: '<App/>'
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe625;</div>
+    </div>
+    <div class="header-input"><span class="iconfont">&#xe632;</span> 输入城市/景点/游玩主题</div>
+    <div class="header-right">城市<span class="iconfont">&#xe6aa;</span></div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeHeader"
+    }
+</script>
+
+<style lang="stylus" scoped>
+  /*   1rem = html   font-size = 50px
+       ~@/  样式引用根目录
+       @/  内容引用根目录
+    */
+  @import "~styles/varibles.styl"
+  .header
+    display flex
+    line-height .86rem
+    background $bgColor
+    color #ffffff
+
+  .header-left
+    width .64rem
+    float left
+
+  .back-icon
+    text-align center
+    font-size .4rem
+
+  .header-input
+    flex 1
+    height .64rem
+    line-height .64rem
+    background #ffffff
+    color #cccccc
+    border-radius .1rem
+    margin-top .12rem
+    margin-left .1rem
+    padding-left .2rem
+
+  .header-right
+    width 1.24rem
+    float right
+    text-align center
+
+
+</style>
