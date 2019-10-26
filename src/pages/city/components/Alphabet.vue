8ae15a0aa98d3776fb99e290fa6e735bdab400e1
一个孤陋寡闻的胖子 <52310567+gulouguawen@users.noreply.github.com>
2019-10-26 15:14:06 +0800

    list switch performance optimizing

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -22,13 +22,20 @@
             letters() {
                 const letters = [];
                 for (let i in this.cities) {
-                    letters.push(i);
+                    letters.push(i)
                 }
                 return letters;
             }
         },
         data() {
-            return {}
+            return {
+                touchStatus: false,
+                startY: 0,
+                timer: null
+            }
+        },
+        updated() {
+            this.startY = this.$refs['A']  [0].offsetTop
         },
         methods: {
             handleLetterClick(e) {
@@ -39,12 +46,17 @@
             },
             handleTouchMove(e) {
                 if (this.touchStatus) {
-                    const startY = this.$refs['A']  [0].offsetTop;
-                    const touchY = e.touches[0].clientY - 75;
-                    const index = Math.floor((touchY - startY) / 20);
-                    if (index >= 0 && index < this.letters.length) {
-                        this.$emit('change', this.letters[index]);
+                    if (this.timer) {
+                        clearTimeout(this.timer)
                     }
+                    this.timer = setTimeout(() => {
+                        const touchY = e.touches[0].clientY - 75;
+                        const index = Math.floor((touchY - this.startY) / 20);
+                        if (index >= 0 && index < this.letters.length) {
+                            this.$emit('change', this.letters[index])
+                        }
+                    }, 16)
+
                 }
             },
             handleTouchEnd() {
