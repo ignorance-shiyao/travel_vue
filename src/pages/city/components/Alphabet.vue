160b2afddd4648601f2f82451b34438f09e5877b
一个孤陋寡闻的胖子 <52310567+gulouguawen@users.noreply.github.com>
2019-10-26 14:51:41 +0800

    list switch

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,12 +1,13 @@
 <template>
   <ul class="list">
-    <li class="item" v-for="(item,key) of cities"
-        :key="key"
+    <li class="item" v-for="item of letters"
+        :key="item"
+        :ref="item"
         @touchstart="handleTouchStart"
         @touchmove="handleTouchMove"
         @touchend="handleTouchEnd"
         @click="handleLetterClick">
-      {{key}}
+      {{item}}
     </li>
   </ul>
 </template>
@@ -16,6 +17,15 @@
         name: "CityAlphabet",
         props: {
             cities: Object
+        },
+        computed: {
+            letters() {
+                const letters = [];
+                for (let i in this.cities) {
+                    letters.push(i);
+                }
+                return letters;
+            }
         },
         data() {
             return {}
@@ -27,13 +37,18 @@
             handleTouchStart() {
                 this.touchStatus = true
             },
-            handleTouchMove() {
+            handleTouchMove(e) {
                 if (this.touchStatus) {
-
+                    const startY = this.$refs['A']  [0].offsetTop;
+                    const touchY = e.touches[0].clientY - 75;
+                    const index = Math.floor((touchY - startY) / 20);
+                    if (index >= 0 && index < this.letters.length) {
+                        this.$emit('change', this.letters[index]);
+                    }
                 }
             },
             handleTouchEnd() {
-
+                this.touchStatus = false
             }
         }
     }
